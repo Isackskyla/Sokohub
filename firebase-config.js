79c9f60529118052da1674c3e25b0dc251613dd7
Isack Skyla <oywerinewton@gmail.com>
2025-07-28 07:44:54 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,7 +1,7 @@
 // firebase-config.js
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.9.0/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.9.0/firebase-analytics.js";
+import { initializeApp } from "";
+import { getAnalytics } from "";
 import {
   getAuth,
   createUserWithEmailAndPassword,
@@ -9,17 +9,17 @@ import {
   signOut,
   onAuthStateChanged,
   sendEmailVerification
-} from "https://www.gstatic.com/firebasejs/11.9.0/firebase-auth.js";
+} from "";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDFtZdjqv03GnogYyQy_OMXxhmnNHLSMrM",
-  authDomain: "sokohub-38103.firebaseapp.com",
-  projectId: "sokohub-38103",
-  storageBucket: "sokohub-38103.appspot.com",
-  messagingSenderId: "765168744986",
-  appId: "1:765168744986:web:bc606817607b5d20a32fa3",
-  measurementId: "G-16TYMXFD9Z"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase app and services
